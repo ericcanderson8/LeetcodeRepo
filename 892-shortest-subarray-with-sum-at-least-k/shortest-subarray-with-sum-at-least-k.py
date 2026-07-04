9513e18bb03d24b83ad7d965520f5d580f3c934f
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-07-04 11:26:15 -0700

    Time: 131 ms (99.78%) | Memory: 26.4 MB (99.78%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        # return shortest subarray with the sum at least k
+        # for this I think we just need a sliding window where the window is the sum k. 
+        # so we will keep on increasing until we get to k length, 
+        # When you add a new item, check how many items you can drop from the back of it, while still keeping it above k
+
+        # we need to have a monotonic q
+        # create a prefix sum tree
+
+        prefix = [0]
+        for i in nums:
+            prefix.append(i + prefix[-1])
+        
+        print(f"Prefix:{prefix}")
+        output = len(nums) + 1
+        q = deque()
+        for i in range(len(prefix)):
+            while q and prefix[q[-1]] > prefix[i]:
+                q.pop()
+            q.append(i)
+            val = prefix[q[-1]] - prefix[q[0]]
+            while val >= k:
+                output = min(output,q[-1]-q[0])
+                q.popleft()
+                val = prefix[q[-1]] - prefix[q[0]]
+        if output == len(nums) + 1:
+            return -1
+        return output
+            
+
+
+
+
+        
+        
