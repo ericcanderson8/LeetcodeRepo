ef0b8a0e15820985749b04043eeac15cce4aff05
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-07-27 19:53:46 -0700

    Time: 394 ms (11.85%) | Memory: 19.6 MB (73.53%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,7 +2,7 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         # find the minimum substring that has all of t
         # keep track of a counter
-        output = ""
+        output = (-1,-1)
 
         minLength = len(s)
         count = Counter(t)
@@ -13,13 +13,16 @@ class Solution:
                 count[s[r]] -= 1
             
             while all(value <= 0 for value in count.values()):
-                if len(output) == 0 or r + 1 - l < len(output):
-                    output = s[l:r+1]
+                if output == (-1,-1) or r + 1 - l < output[1] - output[0]:
+                    output = (l,r+1)
 
                 if s[l] in count:
                     count[s[l]] += 1
                 l += 1
         
-        return output
+        if output == (-1,-1):
+            return ""
+
+        return s[output[0]: output[1]]
 
 
