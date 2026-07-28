090e92fbc3e4ce34e9fd89c341819f2cca87053c
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-07-27 21:17:51 -0700

    Time: 157 ms (92.63%) | Memory: 32.6 MB (5.07%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -2,12 +2,8 @@ import numpy as np
 class Solution:
     
     def smallestPalindrome(self, s: str) -> str:
-        # counts = sorted(Counter(s).items())
-        # half = "".join(c * (k // 2) for c, k in counts)
-        # mid  = "".join(c * (k % 2)  for c, k in counts)
-        # return half + mid + half[::-1]
-        counts = np.bincount(np.frombuffer(s.encode(), np.uint8), minlength=123)[97:].tolist()
-        half = "".join(c * (k // 2) for c, k in zip(ascii_lowercase, counts))
-        mid  = "".join(c * (k % 2)  for c, k in zip(ascii_lowercase, counts))
+        counts = sorted(Counter(s).items())
+        half = "".join(c * (k // 2) for c, k in counts)
+        mid  = "".join(c * (k % 2)  for c, k in counts)
         return half + mid + half[::-1]
         
