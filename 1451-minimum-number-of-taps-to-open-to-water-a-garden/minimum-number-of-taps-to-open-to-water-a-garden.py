9f410b49bb80ed2de46bad28556196c9f957ca38
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-09-05 18:35:13 -0700

    Time: 30 ms (21.27%) | Memory: 20 MB (51.71%) - LeetSync

---
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        # hardest step is saying where you can get from each n
+        # pick maximum of all of the listed options available 
+
+        launches = [-1] * (n + 1)
+        print(launches)
+        for i, val in enumerate(ranges):
+            if i - val < 0:
+                launches[0] = max(launches[0], min(n, i + val))
+            else:
+                print(i - val)
+                launches[i - val] = max(launches[i - val], min(n, i + val))
+        
+        count = 0
+        currentEnd = launches[0]
+        bestJump = 0
+        for i in range(n+1):
+            if i > bestJump:
+                return -1
+
+            if launches[i] > bestJump:
+                bestJump = launches[i]
+            
+            if i == currentEnd:
+                currentEnd = bestJump
+                count += 1
+        return count
