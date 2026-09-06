b3d78c53c33dc384330f6ddd80687834cd2e7bbc
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-09-05 18:36:46 -0700

    Time: 11 ms (90.22%) | Memory: 19.8 MB (89.75%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -4,12 +4,10 @@ class Solution:
         # pick maximum of all of the listed options available 
 
         launches = [-1] * (n + 1)
-        print(launches)
         for i, val in enumerate(ranges):
             if i - val < 0:
                 launches[0] = max(launches[0], min(n, i + val))
             else:
-                print(i - val)
                 launches[i - val] = max(launches[i - val], min(n, i + val))
         
         count = 0
