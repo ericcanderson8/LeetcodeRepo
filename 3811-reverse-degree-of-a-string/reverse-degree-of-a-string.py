3910ce72ce7d66b0435dfc78890fee795b4ec40e
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-09-20 08:38:36 -0700

    Time: 7 ms (70.00%) | Memory: 19.2 MB (87.39%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,8 +1,5 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        # this is pretty simple
-        print(123-ord('a'))
-        print(123-ord('z'))
 
         output = 0
         for i, char in enumerate(s): 
