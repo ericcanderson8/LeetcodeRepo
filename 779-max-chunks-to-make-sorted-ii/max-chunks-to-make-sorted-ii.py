f768c019647ab84925cf8fde97b01eb8c50cc86a
Eric Anderson <64998612+ericcanderson8@users.noreply.github.com>
2026-09-20 10:41:26 -0700

    Time: 5 ms (40.15%) | Memory: 19.6 MB (8.98%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
@@ -1,12 +1,6 @@
 class Solution:
     def maxChunksToSorted(self, arr: list[int]) -> int:
-        # you can create an array to check min and max from left to right
-        # you can go left to right
 
-        # monotonic stack
-
-        # thoughts on this problem create a montonic stack of min, 
-        # if
         output = 0
 
         prefixMax = deque()
@@ -23,13 +17,8 @@ class Solution:
             else:
                 prefixMin.appendleft(prefixMin[0])
 
-        print(f"prefixMin: {prefixMin}")
-        print(f"prefixMax: {prefixMax}")
         for i, num in enumerate(arr):
             if (i == len(arr)-1) or prefixMax[i] <= prefixMin[i+1]:
-                print(i)
                 output += 1
 
-
-        # do you have one that is larger on the left side
         return output            
